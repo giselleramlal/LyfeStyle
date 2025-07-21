4c3c16bd8899d925780f462ab24c82c44515e60d
giselleramlal <sherese101@gmail.com>
2025-07-21 15:12:38 -0400

    initial commit

---
+++ resources/js/Components/MacrosPieChart.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MacrosPieChart from '@/Components/MacrosPieChart.vue'
+</script>
+
+<template>
+  <div class="bg-white rounded-lg shadow p-4">
+    <h2 class="text-xl font-semibold text-gray-900 mb-4">Macronutrient Breakdown</h2>
+    <div class="h-64">
+      <MacrosPieChart />
+    </div>
+  </div>
+</template>
---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <h1>About Page</h1>
+  <p>This is another page using the same layout.</p>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout.vue'
+
+export default {
+  layout: AppLayout,
+}
+</script>
